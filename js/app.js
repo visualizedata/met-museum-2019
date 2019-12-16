f71b7ed360533b213a75a2119bdbadd1b42654e3
Zach Krall <zach@zachkrall.com>
2019-12-16 17:43:56 -0500

    updates with new student projects (#2)
    * updates with student projects, added scroll to top link under each project

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 /*
     Vue.js generates DOM elements for each project
-    using the template tags above.
+    using the template tags declared in the
+    index.html file.
+
+    eventually, it would be nice to have this template
+    entirely declared in JS and bundled with something
+    like webpack.
+
+    also, would be nice to remove jQuery and bootstrap
+    as dependencies.
 */
 var app = new Vue({
   el: '#app',
@@ -9,24 +17,31 @@ var app = new Vue({
     students: []
   },
   mounted(){
+    // when vue component is mounted to DOM
+    // data in the JSON file is loaded into
+    // the arrays declared above
     var self = this;
     $.getJSON('./data.json', function(data){
+      // project data:
       self.projects = data.projects;
+      // student data:
       self.students = data.students;
     });
   },
   methods: {
     getLink(name){
-      var list = this.students.filter(i=> i.name == name);
-      if( list.length > 0){
-        return list[0].url;
-      } else {
-        return '#';
-      }
+      /* the "students" array is used as a lookup object to find personal
+         website links for each student */
+      var list = this.students.filter(i => i.name == name);
+      /* if no link is found, return a '#' which won't break the link */
+      return list.length > 0 ? list[0].url : '#';
     },
     getSlug(title){
-      var slug = title.trim().replace(/([^A-Za-z0-9])/g,'-').toLowerCase();
-      return slug;
+      /* take the title value for each project and generate a safe "slug"
+         which can be used as the <div> id for our scrolling methods */
+      return title.trim()
+                      .toLowerCase()
+                      .replace(/([^A-Za-z0-9])/g,'-');
     },
     getImageURL(filename){
       if (!filename) {
@@ -35,29 +50,31 @@ var app = new Vue({
       return './img/projects/' + filename;
     },
     scrollToView(id, event){
+      // this method is attached to links with
+      // v-on:click to handle our scrolling animation
       event.preventDefault();
       var nav = '#' + id;
+      // if id is false, then scroll to top of page
+      var top = id === false
+                ? 0
+                : $(nav).offset().top - $(nav).height() - 80;
       // calculate scroll from top value
       // e.g.:  (position from top) - (height of element) - (padding)
-      var top = $(nav).offset().top - $(nav).height() - 80;
-      if (nav.length > 1) {
-        $('html, body').stop().animate({
-          scrollTop: top
-        }, 500, 'easeInOutExpo', function() {});
-      }
+      $('html, body').stop()
+                     .animate({ scrollTop: top }, 500, 'easeInOutExpo');
     }
   }
 });
 
 app.$nextTick(function () {
-  /*
-      Vue.nextTick waits for DOM
+  /*  Vue.nextTick waits for DOM
       elements to be rendered before
-      running jQuery actions
-  */
+      running jQuery actions */
+
+  // make app container visible when it's ready
   document.querySelector('#app').style.display = 'block';
 
-  // check for touch screens
+  // check for touch screens and hide related elements
   var hasTouch = window.matchMedia('(pointer: coarse)').matches;
   if (hasTouch) {
     $(".no-touch").hide();
@@ -65,5 +82,4 @@ app.$nextTick(function () {
 
   // activate tooltip
   $('[data-toggle="tooltip"]').tooltip();
-
 });
